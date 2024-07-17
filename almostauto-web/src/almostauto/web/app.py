d98855b7b0f6a0b4c63384641e88ad3710e6d26b
Mike Caley <mike.caley@thomsonreuters.com>
2024-07-17 16:56:03 -0400

    Using database queries

--- almostauto-web/src/almostauto/web/app.py
+++ almostauto-web/src/almostauto/web/app.py
@@ -10,31 +10,16 @@ from litestar.template.config import TemplateConfig
 from litestar import Litestar
 from litestar.static_files import create_static_files_router
 
-from litestar.params import Body
-from litestar.enums import RequestEncodingType
+from litestar.params import Body, Parameter
 
-from litestar import Litestar, get, delete, post
+from litestar import Litestar, get, delete, post, put
 
 from litestar.status_codes import HTTP_200_OK
 
 from pathlib import Path
 
-from dataclasses import dataclass
-
-
-from almostauto.db.tables import Templates
-
-
-@dataclass
-class AATemplate:
-    id: int
-    title: str
-
-
-DATA = {
-    1: AATemplate(1, "Test title")
-}
-DATA_NEXT_ID = 2
+from almostauto.db import tables
+from .models import EditTemplate, EditTemplateDTO, NewTemplate, NewTemplateDTO, ViewTemplate
 
 
 @get("/")
@@ -44,20 +29,8 @@ async def get_home(request: HTMXRequest) -> Template:
 
 @get("/templates")
 async def get_templates(request: HTMXRequest) -> Template:
-    return Template(template_name="pages/templates.html.jinja2", context={"templates": DATA.values()})
-
-
-@dataclass
-class AATemplateSubmit:
-    title: str
-
-
-@post("/templates")
-async def post_template(request: HTMXRequest, data: Annotated[AATemplateSubmit, Body(media_type=RequestEncodingType.URL_ENCODED)]) -> Template:
-    global DATA_NEXT_ID
-    DATA[DATA_NEXT_ID] = AATemplate(DATA_NEXT_ID, AATemplate(DATA_NEXT_ID, data.title))
-    DATA_NEXT_ID += 1
-    return Template(template_name="fragments/templates-view.html.jinja2")
+    templates = await tables.Templates.objects()
+    return Template(template_name="pages/templates.html.jinja2", context={"templates": templates})
 
 
 @get("/templates/new")
@@ -65,21 +38,40 @@ async def get_templates_new(request: HTMXRequest) -> Template:
     return Template(template_name="pages/templates-new.html.jinja2")
 
 
-@delete("/templates/{template_id:int}", status_code=HTTP_200_OK)
-async def delete_template(template_id: int) -> None:
-    if template_id in DATA:
-        del DATA[template_id]
+@post("/templates", dto=NewTemplateDTO)
+async def post_template(request: HTMXRequest, data: NewTemplate) -> Template:
+    template = await tables.Templates.objects().create(title=data.title)
+
+    return HTMXTemplate(
+        push_url=f"{template.id}",
+        template_name="fragments/templates-view.html.jinja2",
+        context={"template": template}
+    )
 
 
 @get("/templates/{template_id:int}")
 async def get_template(request: HTMXRequest, template_id: int) -> Template:
-    template = DATA[template_id]
+    template = await tables.Templates.objects().get(tables.Templates.id == template_id)
+    
     return Template(template_name="pages/templates-id.html.jinja2", context={"template": template})
 
 
-@get("templates/{template_id:int}/edit")
+@put("/templates/{template_id:int}", dto=EditTemplateDTO)
+async def put_template(template_id: int, data: EditTemplate) -> Template:
+    template = await tables.Templates.update(title=data.title).where(tables.Templates.id == template_id)
+
+    return Template(template_name="fragments/templates-view.html.jinja2", context={"template": ViewTemplate(template_id, data.title)})
+
+
+@delete("/templates/{template_id:int}", status_code=HTTP_200_OK)
+async def delete_template(template_id: int) -> None:
+    await tables.Templates.delete().where(tables.Templates.id == template_id)
+
+
+@get("/templates/{template_id:int}/edit")
 async def get_template_id_edit(request: HTMXRequest, template_id: int) -> Template:
-    template = DATA[template_id]
+    template = await tables.Templates.objects().get(tables.Templates.id == template_id)
+
     return Template(template_name="fragments/templates-edit.html.jinja2", context={"template": template})
 
 
@@ -89,10 +81,11 @@ app = Litestar(
         get_home,
         get_templates,
         post_template,
+        put_template,
         get_templates_new,
         get_template,
         delete_template,
-        get_template_id_edit
+        get_template_id_edit,
     ],
     template_config=TemplateConfig(
         directory=Path(__file__).parent / Path("templates"),
---
+++ almostauto-web/src/almostauto/web/models.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from litestar.dto import DataclassDTO
+
+
+@dataclass
+class NewTemplate:
+    title: str
+
+
+@dataclass
+class EditTemplate:
+    title: str
+
+
+@dataclass
+class ViewTemplate:
+    id: int
+    title: str
+
+
+NewTemplateDTO = DataclassDTO[NewTemplate]
+EditTemplateDTO = DataclassDTO[EditTemplate]
+ViewTemplateDTO = DataclassDTO[ViewTemplate]
