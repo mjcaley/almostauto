4d685b217a5293f98c17250cda71a55b3eb7b634
Michael Caley <mjcaley@darkarctic.com>
2024-07-17 22:11:01 -0400

    Reformatted almostauto-db

--- almostauto-db/src/almostauto/db/piccolo_app.py
+++ almostauto-db/src/almostauto/db/piccolo_app.py
@@ -13,12 +13,8 @@ CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 APP_CONFIG = AppConfig(
     app_name="almostauto-db",
-    migrations_folder_path=os.path.join(
-        CURRENT_DIRECTORY, "piccolo_migrations"
-    ),
-    table_classes=table_finder(
-        modules=["almostauto.db.tables"], exclude_imported=True
-    ),
+    migrations_folder_path=os.path.join(CURRENT_DIRECTORY, "piccolo_migrations"),
+    table_classes=table_finder(modules=["almostauto.db.tables"], exclude_imported=True),
     migration_dependencies=[],
     commands=[],
 )
