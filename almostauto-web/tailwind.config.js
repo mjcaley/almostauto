67c46494b51370b2f1f66a8c2b5bb68fa0db38dc
Michael Caley <mjcaley@darkarctic.com>
2024-09-04 22:02:00 -0400

    Styling components

--- almostauto-web/tailwind.config.js
+++ almostauto-web/tailwind.config.js
@@ -1,8 +1,10 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/almostauto/web/components/**/*.jinja"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [daisyui]
+};
